a5f9f76bda1f451f1bc63368ae15154aa3baaf6a
didstriker <didstriker@gmail.com>
2014-11-14 16:26:52 +0100

    Biblioteca dll

--- Juego/CUI/Program.cs
+++ Juego/CUI/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabadanD.Programacion.Eval1.Juego.Logica;
 
-namespace CUI
+namespace RabadanD.Programacion.Eval1.Juego.CUI
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hola Git");
+            int secreto = ModeloJuego.SecretoDeLaLife();
+            Console.WriteLine("El secreto de la Laif es " + secreto);
             Console.ReadLine();
         }
     }
---
+++ Juego/LogicaJuego/LogicaJuego.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabadanD.Programacion.Eval1.Juego.Logica
+{
+    static public class ModeloJuego
+    {
+        public static int SecretoDeLaLife()
+        {
+            return 42;
+        }
+    }
+}
